187b10deb83805e85924a8edc9922572c03bc7b3
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-24 13:34:47 -1000

    fixed / issue

--- getRequest.js
+++ getRequest.js
@@ -4,16 +4,16 @@ const public = "./public";
 
 function getReq(fileURL, response) {
   let path = fileURL === '/' ? `/index.html` : fileURL;
-  fs.readFile(`${public}${path}`, (err, data) => {
+  fs.readFile(`${public}${path}`, 'utf8', (err, data) => {
     if (err) {
       fs.readFile(`${public}/404.html`, (err, data) => {
-        response.write(data.toString().trim());
+        response.write(data);
         response.end(() => {
         })
       })
     } else {
-      response.writeHead(200, { 'Content-Type': `text/${fileURL.substring(fileURL.lastIndexOf('.')+1)}` });
-      response.write(data.toString().trim());
+      response.writeHead(200, { 'Content-Type': `text/${path.substring(path.lastIndexOf('.')+1)}` });
+      response.write(data);
       response.end(() => {
         console.log('Request fulfilled');
       });
