b8ebd031a0d18a8ff9812df55ba79b021eeb5d75
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-23 16:39:20 -1000

    post

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ const server = http.createServer((request, response) => {
   console.log('Incoming request');
   let method = request.method;
   let url = request.url;
-  console.log(url);
-  let version = request.httpVersion;
   //if request method is GET...
   switch(method) {
     case 'GET':
     getReq(url, response);
+    break;
+    case 'POST':
+    postReq(request, response);
+    break;
   }
 })
 
@@ -53,4 +55,40 @@ function getReq(fileURL, response) {
       }
     })
   }
+};
+
+function postReq (request, response) {
+
+  request.on('data', (data) => {
+    let parsedData = qs.parse(data.toString());
+    let newFile = `${request.url.substring(1, request.url.length)}`
+  
+    let newDoc = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>The Elements - ${parsedData.name}</title>
+      <link rel="stylesheet" href="/css/styles.css">
+    </head>
+    <body>
+      <h1>${parsedData.name}</h1>
+      <h2>${parsedData.atomicSymbol}</h2>
+      <h3>Atomic number ${parsedData.atomicNum}</h3>
+      <p>${parsedData.description}</p>
+      <p><a href="/">back</a></p>
+    </body>
+    </html>`
+
+    fs.writeFile(`${public}/${newFile}`, newDoc, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        response.writeHead(200, { 'Content-Type': 'application/json', 'Content-Body': '{ "success" : true }'});
+        response.end(() => {
+          console.log('Write successful');
+        })
+      }
+    })
+  });
 }
+
